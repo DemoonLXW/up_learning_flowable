6ef382de6412061080baf5930fb9115e0ccf6542
shiweibushiwei <924469123@qq.com>
2023-11-12 23:33:23 +0800

    Hide Datasource Properties

--- src/main/java/com/demoon/uplearning/UpLearningApplication.java
+++ src/main/java/com/demoon/uplearning/UpLearningApplication.java
@@ -2,6 +2,7 @@ package com.demoon.uplearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class UpLearningApplication {
