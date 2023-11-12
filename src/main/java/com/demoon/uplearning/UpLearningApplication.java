967556d52d007355de49f71c1e3dada732e84879
shiweibushiwei <924469123@qq.com>
2023-11-12 23:11:03 +0800

    Init Project

---
+++ src/main/java/com/demoon/uplearning/UpLearningApplication.java
@@ -0,0 +1,13 @@
+package com.demoon.uplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demoon/uplearning/UpLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoon.uplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
